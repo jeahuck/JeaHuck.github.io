4175e48c05a3dbf656809d422a04e6d781a516e8
jeahuck <qwpoaslkal@naver.com>
2022-12-07 17:03:29 +0900

    [양재혁] 젠킨스 scm 문구 변경

--- next_jenkens.scm
+++ next_jenkens.scm
@@ -1,17 +1,32 @@
 node{
-
+  stages {
     //init stage 시작
-    stage("init"){
+    stage("init") {
       sh "echo init"
     }
 
     //build stage 시작
-    stage("build"){
+    stage("build") {
       sh "echo 'start build' "
     }
  
     //deploy stage 시작
-    stage("deploy"){
+    stage("deploy") {
       sh "echo 'start deploy' "
     }
+  }
+  post{
+	  always {
+    	echo 'always 문구'
+    }
+    changed {
+      echo 'changed 문구'
+    }
+    failure {
+      echo 'failure 문구'
+    }
+    success {
+      echo 'success 문구'
+    }
+  }
 }
