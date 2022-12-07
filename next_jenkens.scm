01a044c2cd5eb31d393c5edc6916b5fca688111f
jeahuck <qwpoaslkal@naver.com>
2022-12-07 17:48:04 +0900

    [양재혁] github 홈페이지 배포방식으로 진행

--- next_jenkens.scm
+++ next_jenkens.scm
@@ -4,16 +4,17 @@ pipeline {
         stage("Build") {
             steps {
                 sh "npm install"
-                sh "npm run build"
+                //sh "npm run build"
+                sh "npm run deploy"
             }
         }
-        stage("Deploy") {
-            steps {
-                script {
-                    sh 'sudo chmod +x /var/lib/jenkins/deploy/deploy.sh'
-                    sh 'sudo /var/lib/jenkins/deploy/deploy.sh'
-                }
-            }
-        }
+        //stage("Deploy") {
+        //    steps {
+        //        script {
+        //            sh 'sudo chmod +x /var/lib/jenkins/deploy/deploy.sh'
+        //            sh 'sudo /var/lib/jenkins/deploy/deploy.sh'
+        //        }
+        //    }
+        //}
     }
 }
