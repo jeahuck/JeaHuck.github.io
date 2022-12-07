fa625f7162064d2dc601ce9b2ff85d80cf7679ac
jeahuck <qwpoaslkal@naver.com>
2022-12-07 11:05:33 +0900

    [양재혁] 젠킨스 git 연동을 위한 커밋

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const repository = "/next_start/";
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: !debug ? repository : "", // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록 test
 }
 
 module.exports = nextConfig
