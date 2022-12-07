278d9548e4e4612d9ed5c527135759e9e56031c8
jeahuck <qwpoaslkal@naver.com>
2022-12-07 17:30:28 +0900

    [양재혁] sudo 명령어 삭제

--- next_jenkens.scm
+++ next_jenkens.scm
@@ -3,15 +3,15 @@ pipeline {
      stages {
         stage("Build") {
             steps {
-                sh "sudo npm install"
-                sh "sudo npm run build"
+                sh "npm install"
+                sh "npm run build"
             }
         }
         stage("Deploy") {
             steps {
                 script {
-                    sh 'sudo chmod +x /var/lib/jenkins/deploy/deploy.sh'
-                    sh 'sudo /var/lib/jenkins/deploy/deploy.sh'
+                    sh 'chmod +x /var/lib/jenkins/deploy/deploy.sh'
+                    sh '/var/lib/jenkins/deploy/deploy.sh'
                 }
             }
         }
