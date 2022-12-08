0b5f68a32053cec44b7a63a7bf64a7cb7c1bf6fe
jeahuck <qwpoaslkal@naver.com>
2022-12-08 15:00:30 +0900

    [양재혁] 테스트

--- next_jenkens.scm
+++ next_jenkens.scm
@@ -3,6 +3,7 @@ pipeline {
      stages {
         stage("Build") {
             steps {
+                sh "pwd"
                 sh "npm install"
                 //sh "npm run build"
                 sh "npm run deploy-jenkins"
